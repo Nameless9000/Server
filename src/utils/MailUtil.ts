c549e854349d538ef2616d8b863483e28fab5bdb
aspect <airogfx@gmail.com>
2020-11-02 13:07:24 -0500

    Update MailUtil.ts

--- src/utils/MailUtil.ts
+++ src/utils/MailUtil.ts
@@ -11,7 +11,7 @@ async function sendVerificationEmail(user: User) {
     const html = `<h1>Email Verification</h1>
     Thank you for registering on <a href="https://astral.cool">Astral</a>, ${user.username}.<br>
     Please confirm your email with the link below to get started.<br><br>
-    <a href="${process.env.BACKEND_URL}/auth/verify?key=${user.verificationKey}">Verify here.</a>`;
+    <a href="${process.env.BACKEND_URL}/auth/verify?key=${user.emailVerificationKey}">Verify here.</a>`;
 
     const msg = {
         to: user.email,
