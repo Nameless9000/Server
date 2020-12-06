43b99563a7f452ba08982d7202871d7d9746d9ea
aspect <airogfx@gmail.com>
2020-12-06 11:16:56 -0500

    Finish invites router & invite generation

--- src/routes/InvitesRouter.ts
+++ src/routes/InvitesRouter.ts
@@ -6,27 +6,6 @@ import UserModel from '../models/UserModel';
 import { generateInvite } from '../utils/GenerateUtil';
 const router = Router();
 
-router.get('/', AdminMiddleware, async (_req: Request, res: Response) => {
-    const count = await InviteModel.countDocuments();
-
-    const invites = await InviteModel.find({})
-        .select('-__v');
-
-    const redeemedInvites = await InviteModel.find({ redeemed: true })
-        .select('-__v');
-
-    const unusableInvites = await InviteModel.find({ useable: false })
-        .select('-__v');
-
-    res.json({
-        success: true,
-        count,
-        invites,
-        redeemedInvites,
-        unusableInvites,
-    });
-});
-
 router.post('/', AuthMiddleware, async (req: Request, res: Response) => {
     const { user } = req;
 
@@ -58,13 +37,29 @@ router.post('/', AuthMiddleware, async (req: Request, res: Response) => {
     });
 });
 
+router.get('/', AdminMiddleware, async (_req: Request, res: Response) => {
+    const count = await InviteModel.countDocuments();
+
+    const invites = await InviteModel.find({})
+        .select('-__v');
+
+    const redeemedInvites = await InviteModel.find({ redeemed: true })
+        .select('-__v');
+
+    const unusableInvites = await InviteModel.find({ useable: false })
+        .select('-__v');
+
+    res.json({
+        success: true,
+        count,
+        invites,
+        redeemedInvites,
+        unusableInvites,
+    });
+});
+
 router.get('/:code', AdminMiddleware, async (req: Request, res: Response) => {
     const code = req.params.code as string;
-
-    if (!code) return res.status(400).json({
-        success: false,
-        error: 'provide a code',
-    });
 
     const invite = await InviteModel.findById(code)
         .select('-__v');
@@ -78,6 +73,33 @@ router.get('/:code', AdminMiddleware, async (req: Request, res: Response) => {
         success: true,
         invite,
     });
+});
+
+router.delete('/:code', AdminMiddleware, async (req: Request, res: Response) => {
+    const code = req.params.code as string;
+
+    const invite = await InviteModel.findById(code);
+
+    if (!invite) return res.status(404).json({
+        success: false,
+        error: 'invalid invite code',
+    });
+
+    try {
+        await InviteModel.findByIdAndUpdate(invite._id, {
+            useable: false,
+        });
+
+        res.status(200).json({
+            success: true,
+            message: 'deleted invite successfully',
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            error: err.message,
+        });
+    }
 });
 
 export default router;
